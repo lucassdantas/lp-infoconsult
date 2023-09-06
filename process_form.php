934f1fecbf2d8050b6dfb503db5637547122bcf3
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-06 13:14:38 -0300

    add email acess

--- process_form.php
+++ process_form.php
@@ -20,18 +20,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sendBtn'])) {
     }
     
     require 'mailer/PHPMailerAutoload.php';
+    require_once 'includes/emailAcess.php';
     $mail = new PHPMailer();
     $mail->isSMTP();
     $mail->Host = 'email-ssl.com.br';
     $mail->SMTPAuth = true;
     $mail->SMTPSecure = 'ssl';
-    $mail->Username ='';
-    $mail->Password = '';
+    $mail->Username = $email;
+    $mail->Password = $pass;
     $mail->Port = '465';
     
     $mail->setFrom($email);
     $email->addReplyTo($email);
-    $addAddress('lucasdantas.rdmarketingdigital@gmail.com');
+    $email->$addAddress('lucasdantas.rdmarketingdigital@gmail.com');
 
     $mail->Subject = "Novo Lead da Landing Page";
     $mail->Body = " 
@@ -44,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sendBtn'])) {
         echo "Erro ao enviar mensagem.".$mail->ErrorInfo;
     } else {
         echo "E-mail enviado com sucesso";
-        header('location: ./thank-you.php');
+        header('location: ./obrigado.php');
     }
 } else {
     echo "Você não tem permissão para acessar esta página.";
